62a71e59b76e4648f60bf71a4e7a0ef4b9ceb9a6
petrofcz <petrof91@gmail.com>
2020-04-04 19:44:48 +0200

    paths fix

--- backend/public/index.php
+++ backend/public/index.php
@@ -6,7 +6,7 @@ use Slim\App;
 // todo vymysli ini_set('display_errors', 1); - nejlepe v dockeru
 
 /** @var \DI\Container $container */
-$container = require __DIR__ . '../src/bootstrap.php';
+$container = require __DIR__ . '/../src/bootstrap.php';
 
 // Create Slim app
 $app = $container->get(App::class);
--- backend/src/bootstrap.php
+++ backend/src/bootstrap.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 use App\DI\ContainerFactory;
 
-require_once __DIR__ . '../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 $diContainerFactory = new ContainerFactory();
 
