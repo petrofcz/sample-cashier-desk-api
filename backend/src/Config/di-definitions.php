bc28dd81d8787d1c46ec88f69843707bd1db5744
petrofcz <petrof91@gmail.com>
2020-04-06 02:00:24 +0200

    di + config

--- backend/src/Config/di-definitions.php
+++ backend/src/Config/di-definitions.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 use App\Auth\ApiKeyRequestAuthenticator;
 use App\Common\JSONDecoderMiddleware;
 use App\Auth\RequestAuthenticatorInterface;
+use App\Logging\ResourceTrafficLogger;
+use App\Logging\TrafficLoggerInterface;
+use App\Logging\TrafficLogMiddleware;
 use App\Payment\Slim\PaymentSlimConfigurator;
 use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseFactoryInterface;
@@ -11,16 +14,28 @@ use Slim\App;
 use Slim\Factory\AppFactory;
 use Slim\Psr7\Factory\ResponseFactory;
 
+const ENV_MONGO_URL = 'MONGO_URL';
+const ENV_MONGO_DATABASE = 'MONGO_DATABASE';
+const ENV_DEBUG = 'DEBUG';
+
 return [
 
-    // Response factory
-    ResponseFactoryInterface::class => function(ContainerInterface $container) {
-        return $container->get(ResponseFactory::class);
-    },
+    // Api version
+    'api.version'   =>  1,
 
-    // Authenticator
-    RequestAuthenticatorInterface::class => function(ContainerInterface $container) {
-        return $container->get(ApiKeyRequestAuthenticator::class);
+    // Register mongo constants
+    'mongo.url' =>  DI\env(ENV_MONGO_URL, '127.0.0.1'),
+    'mongo.database' =>  DI\env(ENV_MONGO_DATABASE, 'cashierDesk'),
+
+    'debug' => DI\env(ENV_DEBUG, false),
+
+    // Mongo client factory
+    \MongoDB\Database::class => function(ContainerInterface $container) {
+        $client = new MongoDB\Client(
+            $container->get('mongo.url')
+        );
+        $database = $client->selectDatabase($container->get('mongo.database'));
+        return $database;
     },
 
     // App factory
@@ -36,6 +51,27 @@ return [
         // Setup automatic input content decoding
         $app->add(JSONDecoderMiddleware::class);
 
+        // Setup logging
+        $app->add(TrafficLogMiddleware::class);
+
+        // Setup error reporting
+        $app->addErrorMiddleware($container->get('debug') ? true : false, true, true);
+
+        $app->setBasePath(sprintf('/api/v%d', $container->get('api.version')));
+
         return $app;
+    },
+
+    // Traffic logging
+    TrafficLoggerInterface::class => DI\create(ResourceTrafficLogger::class)->constructor(fopen('php://stdout', 'w')),
+
+    // Response factory
+    ResponseFactoryInterface::class => function(ContainerInterface $container) {
+        return $container->get(ResponseFactory::class);
+    },
+
+    // Authenticator
+    RequestAuthenticatorInterface::class => function(ContainerInterface $container) {
+        return $container->get(ApiKeyRequestAuthenticator::class);
     },
 ];
